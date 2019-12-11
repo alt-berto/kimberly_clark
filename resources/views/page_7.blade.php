74d66efd02622764b427bfa3ee906bfec1f06fcf
Nicolas Lopez <nicolas@piso83digital.com>
2019-12-11 15:50:04 -0600

    Update page_7.blade.php
    nueva pag

--- resources/views/page_7.blade.php
+++ resources/views/page_7.blade.php
@@ -10,19 +10,58 @@
         <!-- Sidebar -->
         <side-menu-component page-num="{{ $page }}"></side-menu-component>
         <!-- -->
-        <div id="content-wrapper">
-            <nav class="navbar static-top">    
-                <h1 class="mainTitle">--</h1>    
-                <img class="iconLogo" src="{{ asset('imgs/icon-logo.svg') }}" alt="">
+        <div id="content-wrapper" class="boxBlue">
+
+            <nav class="navbar static-top">
+
+                <h1 class="mainTitle">Procesos de cuentas por pagar</h1>
+
             </nav>
-            <div class="container-fluid contentBox">
-                <div class="content3">
-                    <div class="topLineColor"></div>
-                    
-                </div><!-- /.container-fluid -->
-            </div><!-- /.container-fluid -->        
+            <div class="container-fluid">
+                <div class="row circleBox">
+                    <div class="col-md-7 circleOptions">
+                        <img src="{{ asset('imgs/circle-shape.png') }}" alt="" style="width: 100%; visibility: hidden"/>
+
+                        <div class="circlesEachOption">
+                            <a class="ventas buble" href="">
+
+                                <img src="{{ asset('imgs/cartera-icon.svg') }}" alt="">
+                            </a>
+                            <a class="apply buble" href="">
+                                <img src="{{ asset('imgs/dividendos-icon.svg') }}" alt="">
+                            </a>
+                            <a class="resolucion buble" href="">
+                                <img src="{{ asset('imgs/intercompany-cartera-icon.svg') }}" alt="">
+                            </a>
+                            <a class="recaudo buble" href="">
+                                <img src="{{ asset('imgs/proviciones-icon.svg') }}" alt="">
+                            </a>
+                            <a class="facturacion buble" href="">
+                                <img src="{{ asset('imgs/cartera-empleados-icon.svg') }}" alt="">
+                            </a>
+                            <a class="orden buble" href="">
+                                <img src="{{ asset('imgs/impuestos-icon.svg') }}" alt="">
+                            </a>
+                            <a class="anticipo buble" href="">
+                                <img src="{{ asset('imgs/anticipos-icon.svg') }}" alt="">
+                            </a>
+
+                        </div>
+                        <div class="mainCuentas">
+                            <img src="{{ asset('imgs/icon-cuentas-cobrar.svg') }}" alt="">
+                        </div>
+
+                    </div>
+                    <div class="col-md-5 mainBG">
+                        <img src="{{ asset('imgs/BG-PAGE-7.png') }}" alt="" style="width: 100%">
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+
+
+
         </div>
-        <!-- /.content-wrapper -->        
+        <!-- /.content-wrapper -->
     </div>
     <!-- /#wrapper -->
 
