09621470158dbdd7ab1b3b52c13117988499cd08
Nicolas Lopez <nicolas@piso83digital.com>
2020-01-23 16:09:10 -0600

    Nuevos cambios
    Nuevos cambios

--- resources/views/page_19.blade.php
+++ resources/views/page_19.blade.php
@@ -19,7 +19,7 @@
             quices="{{ true }}"
             evaluaciones="{{ false }}"
             page-num="{{ $page }}">
-        </side-menu-component><side-menu-component page-num="{{ $page }}"></side-menu-component>
+        </side-menu-component>
         <!-- -->
         <div id="content-wrapper">
             <nav class="navbar static-top">    
